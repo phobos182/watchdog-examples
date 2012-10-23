49634ee3c609c635e59e6029fb1aff7d8c37cb00
Jeremy Carroll <phobos182@gmail.com>
2012-10-23 13:26:20 -0700

    Removed duration timers. Failure counters will determine timers. Ex: check every 1 minute, 3 failures = 3 minutes

--- watchdog.py
+++ watchdog.py
@@ -1,38 +1,39 @@
 #!/usr/bin/env python
-# 2012 - Jeremy Carroll
-# WatchDog testing
-# Read from a file. If "ok" in file, then healthy.
-# Else failed. Test extending DSL with a Tiered remediation
-# script to take multiple actions based on failure count.
-# State is stored in the rule object as a hack, investigating
-# other methods
 from watchdog import *
 from networkinterface import *
 
 
-
 class InterfaceSpeed(Rule):
     """Rule to check to ensure that active linkes are 1000/full."""
-    def __init__(self, timeout=Time.s(10), freq=Time.m(60), fail=(), success=()):
+    def __init__(self, timeout=Time.s(10), freq=Time.m(1), fail=(), success=(), speed="1000", duplex="full"):
         Rule.__init__(self, freq, fail, success)
+        # State about the rules failure count, and start of error.
+        # This will help in our Tiered policy to keep track of the 
+        # current status of the failure.
         self.failcount = []
+        self.speed = speed
+        self.duplex = duplex
 
 
     def run(self):
-        f = open("/tmp/testfile", "r")
-        a = f.readlines()
-        f.close()
-        if "ok" in a[0].rstrip():
-            print 'Healthy'
-            return Success(None)
-        else:
-            return Failure("File: %s" % (a))
+        iface = NetworkInterface()
+        for i in iface.interfaces:
+            if 'lo' in i:
+                continue
+            ifspeed = getattr(iface, "%s_speed" % (i))
+            ifduplex = getattr(iface, "%s_duplex" % (i))
+            ifup = getattr(iface, "%s_operstate" % (i))
+            if ifup == "up":
+                if ifspeed != self.speed or ifduplex != self.duplex:
+                    return Failure("Interface %s test failed. Speed: %s, Duplex: %s" % (ifspeed, ifduplex))
+                Logger.Info("Interface %s - Speed: %s, Duplex: %s" % (i, ifspeed, ifduplex))
+        return Success(None)
 
 
 class Tier(Action):
     """Triggers an action if there are > N errors, and < Y errors
         within a time lapse. Useful for tiering conditions."""
-    def __init__(self, actions, description="undef", errors=5, unmonitor=10, during=30 * 1000):
+    def __init__(self, actions, description="undef", errors=5, unmonitor=10):
         Action.__init__(self)
         if not (type(actions) in (tuple, list)):
             actions = tuple([actions])
@@ -40,22 +41,18 @@ class Tier(Action):
         self.description = description
         self.errors = errors
         self.unmonitor = unmonitor
-        self.during = during
-        self.errorValues = []
-        self.errorStartTime = 0
 
 
     def run(self, monitor, service, rule, runner):
         """When the number of errors is reached within the period, the given
         actions are triggered."""
-        if not rule.failcount:
-            self.errorStartTime = now()
+        # De-duplcate error counts due to multiple threads.
+        # Add lastRun (epoch) to list, exists in array - skip it.
         if rule.lastRun not in rule.failcount:
             rule.failcount.append(rule.lastRun)
-        elapsed_time = now() - self.errorStartTime
-        print "\nEval: %s :: Min: %s, Max: %s, Iteration: %s, Elapsed: %s, During: %s" % (self.description, self.errors, self.unmonitor, len(rule.failcount), elapsed_time, self.during)
-        if len(rule.failcount) >= self.errors and len(rule.failcount) <= self.unmonitor and elapsed_time <= self.during:
-            print "\n    Executing: %s, Iteration: %x" % (self.description, len(rule.failcount))
+        Logger.Debug("Eval: %s :: Min: %s, Max: %s, Iteration: %s" % (self.description, self.errors, self.unmonitor, len(rule.failcount)))
+        if len(rule.failcount) >= self.errors and len(rule.failcount) <= self.unmonitor:
+            Logger.Debug("Executing: %s, Iteration: %x" % (self.description, len(rule.failcount)))
             for action in self.actions:
                 action.run(monitor, service, rule, runner)
 
@@ -67,7 +64,7 @@ class ResetTier(Action):
 
 
     def run(self, monitor, service, rule, runner):
-        """Reset counter."""
+        """Reset failure counter."""
         try:
             rule.failcount = []
         except Exception:
@@ -79,24 +76,22 @@ Monitor (
         name    = "network-health",
         monitor = (
             InterfaceSpeed(
-                freq=Time.s(10),
+                freq=Time.m(1),
                 fail=[
                     Tier(
-                        errors=1,
                         description="Restart Interface",
-                        unmonitor=2,
-                        during=Time.s(300),
+                        errors=1,
+                        unmonitor=3,
                         actions=[
-                            Run("touch /tmp/remedy")
+                            Run("/etc/init.d/networking restart")
                         ]
                     ),
                     Tier(
-                        description="Stop prescriptions",
-                        errors=3,
-                        unmonitor=3,
-                        during=Time.s(300),
+                        description="Send fix API call",
+                        errors=4,
+                        unmonitor=4,
                         actions=[
-                            Run("touch /tmp/remedy_last")
+                            Run("echo Could not remedy network interface - $(date +%s) > /var/log/watchdog.err")
                         ]
                     )
                 ],
